aa2e85cff00910de7861f9344b454481f7080b9a
Ohan Smit <ohan@integrative.co.za>
2019-05-27 13:02:29 +0200

    Add project files.

---
+++ WUCharts/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+using WUCharts.Models;
+
+namespace WUCharts.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This will serve as a replacemet for my current weather34 template hosted on https://cptsats.co.za/pws";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public class AgeInfo
+        {
+
+            public string age;
+            public int population;
+
+            public AgeInfo(string prmAge, int prmPop)
+            {
+                this.age = prmAge;
+                this.population = prmPop;
+            }
+
+        }
+
+
+        // https://gunnarpeipman.com/aspnet/aspnet-core-node-d3js/
+        public async Task<IActionResult> Chart([FromServices] INodeServices nodeServices)
+        {
+            var options = new { width = 400, height = 200 };
+
+            List<AgeInfo> ls = new List<AgeInfo>();
+            ls.Add(new AgeInfo("<5", 2704659));
+            ls.Add(new AgeInfo("5-13", 4499890));
+            ls.Add(new AgeInfo("14-17", 2159981));
+            ls.Add(new AgeInfo("18-24", 3853788));
+            ls.Add(new AgeInfo("25-44", 14106543));
+            ls.Add(new AgeInfo("45-64", 8819342));
+            ls.Add(new AgeInfo("≥65", 612463));
+
+
+            // string markup = await nodeServices.InvokeAsync<string>("Node/d3Pie.js", options, data);
+
+            ViewData["ChartImage"] = await nodeServices.InvokeAsync<string>("Node/d3chart.js", options, ls);
+
+            return View();
+        }
+    }
+}
---
+++ WURequest/Controllers/ValuesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace WURequest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public ValuesController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<string>> GetAsync()
+        {
+            try
+            {
+                var stationId = "IKRAAIFO3";
+                var format = "json";
+                var units = 'm';
+                var personalaccesstoken = "d4748acffd2e4d8ab48acffd2e7d8abc";
+                string webRootPath = _hostingEnvironment.WebRootPath;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    using (HttpResponseMessage response = await client.GetAsync(
+                                string.Format("https://api.weather.com/v2/pws/observations/current?stationId={0}&format={1}&units={2}&apiKey={3}", stationId, format, units, personalaccesstoken)))
+                    {
+                        try
+                        {
+                            response.EnsureSuccessStatusCode();
+                            string responseBody = await response.Content.ReadAsStringAsync();
+                            using (StreamWriter outputFile = new StreamWriter(Path.Combine(webRootPath + "/logs/wudata.txt"), append: true))
+                            {
+                                await outputFile.WriteLineAsync(responseBody);
+                            }
+                            return responseBody;
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                        }
+                        return "NO DATA";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+
+
+    }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartsController : ControllerBase
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public ChartsController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpGet]
+        public ActionResult<List<object>> Get()
+        {
+
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            //var lineCount = System.IO.File.ReadLines(webRootPath + "/logs/wudata11.txt").Count();
+            var list = new List<object>();
+            var fileitems = System.IO.File.ReadLines(webRootPath + "/logs/wudata11.txt");
+            foreach (var item in fileitems)
+            {
+                JObject jObj = JObject.Parse(item);
+                var temperature = jObj["observations"][0]["metric"]["temp"].ToString();
+                var time = jObj["observations"][0]["obsTimeLocal"].ToString();
+                list.Add(time + "," + temperature);
+            }
+            return list;
+
+        }
+    }
+}
